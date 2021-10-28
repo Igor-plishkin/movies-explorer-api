8e2c17cbe6f411b4475cfdbf68d357b7874c7fca
Igor-plishkin <Plisha.jr@yandex.ru>
2021-10-28 21:34:11 +0500

    add auth middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const auth = require("./middlewares/auth");
 
 const { PORT = 3000 } = process.env;
 
@@ -12,6 +13,7 @@ mongoose.connect("mongodb://localhost:27017/moviesdb", {
   useUnifiedTopology: true,
 });
 
+app.use(auth);
 app.use("/", require("./routes/routes"));
 
 app.listen(PORT, () => {
