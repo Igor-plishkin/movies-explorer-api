df51fe9a8e2c84558688cb1b969ba03d11401372
Igor-plishkin <Plisha.jr@yandex.ru>
2021-10-28 21:38:30 +0500

    add cookie-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const auth = require("./middlewares/auth");
 
 const { PORT = 3000 } = process.env;
@@ -12,6 +13,8 @@ mongoose.connect("mongodb://localhost:27017/moviesdb", {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+app.use(cookieParser());
 
 app.use(auth);
 app.use("/", require("./routes/routes"));
