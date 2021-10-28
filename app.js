4400fb7ce4041bea2586f48f50419f15ca8aed3e
Igor-plishkin <Plisha.jr@yandex.ru>
2021-10-28 21:47:25 +0500

    add body-parser

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
 const auth = require("./middlewares/auth");
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/moviesdb", {
   useNewUrlParser: true,
