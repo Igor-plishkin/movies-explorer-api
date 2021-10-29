ece8b1f43fccf850ec104f2d1b1d7592f2718775
Igor-plishkin <Plisha.jr@yandex.ru>
2021-10-30 02:52:53 +0500

    added auth in app

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const {
+  celebrate,
+  Joi,
+  errors,
+} = require("celebrate");
 
+const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
 
 const { PORT = 3000 } = process.env;
@@ -21,9 +27,23 @@ mongoose.connect("mongodb://localhost:27017/moviesdb", {
 
 app.use(cookieParser());
 
+app.post("/signin", celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post("/signup", celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+  }),
+}), createUser);
 app.use(auth);
 app.use("/", require("./routes/routes"));
 
+app.use(errors());
 app.listen(PORT, () => {
   console.log("start server");
 });
